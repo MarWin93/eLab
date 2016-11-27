2371ec705155e639986743cdda26e7232ffa9826
magkwidz <magda.kwidzinska@gmail.com>
2016-11-27 11:56:01 +0100

    Fix chatting

--- WebSites/eLab.Client/app/src/topics/ChatHelper.js
+++ WebSites/eLab.Client/app/src/topics/ChatHelper.js
@@ -1,57 +1,8 @@
-﻿angular.module('eLabApp').service('chatHelper', function () {
+﻿angular.module('eLabApp').service('chatHelper', function ($rootScope) {
     // Self Object
-    var chat = this;
-    $scope.channel = 'messages-channel';
+    
 
-    chat.current_user = "Lolek";
-    // Sent Indicator
-    chat.status = "";
-    chat.messages = [
-          {
-              "date": "2017-04-23T22:00:00.000Z",
-              "content": "lalalalallalalal",
-              "sender": "Marian"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "hej",
-              "sender": "Lolek"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "fdsffdfsdd",
-              "sender": "Jurek"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "hej",
-              "sender": "Lolek"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "hej",
-              "sender": "Lolek"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "fdsffdfsdd",
-              "sender": "Jurek"
-          },
-          {
-              "date": "2016-04-23T00:00:00.000Z",
-              "content": "hej",
-              "sender": "Lolek"
-          }
-    ];
-
-    chat.send = function () {
-        console.log(chat.textbox);
-        chat.status = "sending";
-        chat.textbox = "";
-        setTimeout(function () { chat.status = "" }, 1200);
-    };
-
+    return self;
 });
-
 
 
