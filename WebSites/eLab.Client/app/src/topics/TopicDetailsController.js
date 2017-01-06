f5c42528f3d84abc90d0a29417dd0033feaf2be4
magkwidz <magda.kwidzinska@gmail.com>
2017-01-06 13:08:29 +0100

    topic details

--- WebSites/eLab.Client/app/src/topics/TopicDetailsController.js
+++ WebSites/eLab.Client/app/src/topics/TopicDetailsController.js
@@ -75,26 +75,104 @@
     };
 
     vm.fileDownload = function (file) {
-        $http.get(API_PATH + 'topics/' + vm.topic.id + '/download/' + file.id)
-            .then(function (response) {
+        // Use an arraybuffer
+        $http.get(API_PATH + 'courses/' + vm.course.id + '/download/' + file.id, { responseType: 'arraybuffer' })
+        .success(function (data, status, headers) {
 
-                var byteNumbers = new Array(response.data.length);
-                for (var i = 0; i < response.data.length; i++) {
-                    byteNumbers[i] = response.data.charCodeAt(i);
+            var octetStreamMime = 'application/octet-stream';
+            var success = false;
+
+            // Get the headers
+            headers = headers();
+
+            // Get the filename from the x-filename header or default to "download.bin"
+            var filename = headers['x-filename'] || file.name;
+
+            // Determine the content type from the header or default to "application/octet-stream"
+            var contentType = headers['content-type'] || octetStreamMime;
+
+            try {
+                // Try using msSaveBlob if supported
+                console.log("Trying saveBlob method ...");
+                var blob = new Blob([data], { type: contentType });
+                if (navigator.msSaveBlob)
+                    navigator.msSaveBlob(blob, filename);
+                else {
+                    // Try using other saveBlob implementations, if available
+                    var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
+                    if (saveBlob === undefined) throw "Not supported";
+                    saveBlob(blob, filename);
                 }
-                var byteArray = new Uint8Array(byteNumbers);
+                console.log("saveBlob succeeded");
+                success = true;
+            } catch (ex) {
+                console.log("saveBlob method failed with the following exception:");
+                console.log(ex);
+            }
 
-                var a = window.document.createElement('a');
-                a.href = window.URL.createObjectURL(new Blob([byteArray], { type: 'octet/stream' }));
-                a.download = file.name;
-                document.body.appendChild(a);
-                a.click();
-                document.body.removeChild(a);
-            })
-            .catch(function (error) {
-                console.log("Failed to delete download file");
-                console.log(error);
-            });
+            if (!success) {
+                // Get the blob url creator
+                var urlCreator = window.URL || window.webkitURL || window.mozURL || window.msURL;
+                if (urlCreator) {
+                    // Try to use a download link
+                    var link = document.createElement('a');
+                    if ('download' in link) {
+                        // Try to simulate a click
+                        try {
+                            // Prepare a blob URL
+                            console.log("Trying download link method with simulated click ...");
+                            var blob = new Blob([data], { type: contentType });
+                            var url = urlCreator.createObjectURL(blob);
+                            link.setAttribute('href', url);
+
+                            // Set the download attribute (Supported in Chrome 14+ / Firefox 20+)
+                            link.setAttribute("download", filename);
+
+                            // Simulate clicking the download link
+                            var event = document.createEvent('MouseEvents');
+                            event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+                            link.dispatchEvent(event);
+                            console.log("Download link method with simulated click succeeded");
+                            success = true;
+
+                        } catch (ex) {
+                            console.log("Download link method with simulated click failed with the following exception:");
+                            console.log(ex);
+                        }
+                    }
+
+                    if (!success) {
+                        // Fallback to window.location method
+                        try {
+                            // Prepare a blob URL
+                            // Use application/octet-stream when using window.location to force download
+                            console.log("Trying download link method with window.location ...");
+                            var blob = new Blob([data], { type: octetStreamMime });
+                            var url = urlCreator.createObjectURL(blob);
+                            window.location = url;
+                            console.log("Download link method with window.location succeeded");
+                            success = true;
+                        } catch (ex) {
+                            console.log("Download link method with window.location failed with the following exception:");
+                            console.log(ex);
+                        }
+                    }
+
+                }
+            }
+
+            if (!success) {
+                // Fallback to window.open method
+                console.log("No methods worked for saving the arraybuffer, using last resort window.open");
+                window.open(httpPath, '_blank', '');
+            }
+        })
+        .error(function (data, status) {
+            console.log("Request failed with status: " + status);
+
+            // Optionally write the error out to scope
+            $scope.errorDetails = "Request failed with status: " + status;
+        });
     };
 
     vm.present = function (pdfId) {
@@ -103,6 +181,12 @@
             fileId: pdfId
         });
     };
+
+    vm.isPdf = function (file) {
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+        console.log(file.name.slice(-3));
+        return (file.name.slice(-3)=='pdf' || file.name.slice(-3)=='PDF' || file.name.slice(-3)=='Pdf');
+    }
 
     vm.changePDF = function (pdfId) {
         $scope.ctrl.pdfURL = API_PATH + 'topics/' + topic.id + '/show/' + pdfId;
